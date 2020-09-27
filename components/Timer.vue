a014d819d650243c6773e71f3ac6e5239fd0d3a8
Eric Raslich <rasliche@gmail.com>
2020-09-27 16:02:53 -0400

    Initial Commit

---
+++ components/Timer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <span>
+          {{ displayTime }}
+      </span>
+      <button @click="runTimer">Start</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            timeRemaining: 25 * 60
+        }
+    },
+    computed: {
+        displayTime() {
+            const minutes = Math.floor(this.timeRemaining / 60);
+            const seconds = this.timeRemaining % 60;
+            return `${minutes}:${seconds > 9 ? seconds : '0'+ seconds}`
+        }
+    },
+    methods: {
+        runTimer() {
+            this.timerInterval = setInterval(() => {
+                this.timeRemaining--
+            }, 1000)
+        }
+    }
+}
+</script>
