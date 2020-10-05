5a0b7af91802ff21ec729a407cf531d6ffb7b385
Eric Raslich <rasliche@gmail.com>
2020-10-05 08:40:05 -0400

    New Tailwind Config file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+    theme: {
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
